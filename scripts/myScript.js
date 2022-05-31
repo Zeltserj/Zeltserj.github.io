7510aa8dd1f124cfb93d4861be096b30d56a67cd
Jonathan Zeltser <zeltserj@fb.com>
2022-05-31 17:19:02 +0300

    Fix clear bug

--- scripts/myScript.js
+++ scripts/myScript.js
@@ -8,8 +8,10 @@ console.log(buttonList)
 let result = 0;
 for (const b of buttonList){
   b.onclick = function() {
+  if (b.id != "clear"){
   let first = Number(prompt('Please first number'));
   let second = Number(prompt('Please second number'));
+  }
   console.log(first, second)
   switch (b.id){
     case "add":
