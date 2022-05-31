55acadc41a0278b59be387677da8fa0186f061f6
Jonathan Zeltser <zeltserj@fb.com>
2022-05-31 17:22:56 +0300

    Fix clear prompt

--- scripts/myScript.js
+++ scripts/myScript.js
@@ -8,11 +8,13 @@ console.log(buttonList)
 let result = 0;
 for (const b of buttonList){
   b.onclick = function() {
-  if (b.id != "clear"){
-  let first = Number(prompt('Please first number'));
-  let second = Number(prompt('Please second number'));
-  }
+  let first=0;
+  let second=0;
+  if (b.id !== "clear"){
+  first = Number(prompt('Please first number'));
+  second = Number(prompt('Please second number'));
   console.log(first, second)
+  }
   switch (b.id){
     case "add":
       result = first + second;
