b9d9a7faa8b99bbac0b24d4d303dec8be5a085d3
Jonathan Zeltser <zeltserj@fb.com>
2022-05-31 17:11:24 +0300

    Initial commit

---
+++ scripts/myScript.js
@@ -0,0 +1,35 @@
+const body = document.body
+let buttonList = document.querySelectorAll("button");
+let myHeading = document.querySelector('h1');
+let resultText = document.querySelector('p')
+
+console.log(buttonList)
+
+let result = 0;
+for (const b of buttonList){
+  b.onclick = function() {
+  let first = Number(prompt('Please first number'));
+  let second = Number(prompt('Please second number'));
+  console.log(first, second)
+  switch (b.id){
+    case "add":
+      result = first + second;
+      break;
+    case "subtract":
+      result = first - second;
+      break;
+    case "divide":
+      result = first / second;
+      break;
+    case "multiply":
+      result = first * second;
+      break;
+    case "clear":
+      result = 0;
+      break;
+  }
+  console.log(result);
+  resultText.textContent = "Result= " + result;
+}
+
+}
