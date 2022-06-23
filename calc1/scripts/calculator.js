47b1331f7f8ec7edfe9982db138ca9dc0e7974d4
Jonathan Zeltser <zeltserj@fb.com>
2022-06-23 14:50:41 +0300

    pivot to dinner generator

--- calc1/scripts/calculator.js
+++ calc1/scripts/calculator.js
@@ -1,38 +1,11 @@
 const body = document.body
-let buttonList = document.querySelectorAll("button");
-let resultText = document.querySelector('p')
+let b = document.querySelector("button");
+let resultText = document.querySelector('p');
+let selection = document.querySelector('#falvors');
+console.log(selection)
 
-console.log(buttonList)
-
-let result = 0;
-for (const b of buttonList){
-  b.onclick = function() {
-  let first = 0;
-  let second = 0;
-  if (b.id !== "clear"){
-  first = Number(prompt('Please first number'));
-  second = Number(prompt('Please second number'));
-  console.log(first, second)
-  }
-  switch (b.id){
-    case "add":
-      result = first + second;
-      break;
-    case "subtract":
-      result = first - second;
-      break;
-    case "divide":
-      result = first / second;
-      break;
-    case "multiply":
-      result = first * second;
-      break;
-    case "clear":
-      result = 0;
-      break;
-  }
-  console.log(result);
-  resultText.textContent = "Result= " + result;
-}
-
-}
+b.onclick = (event) => {
+            event.preventDefault();
+            // show the selected index
+            resultText.innerHTML = "For a "+ selection.value + " dish, please see go <a href='https://food.walla.co.il/item/3361666'>here</a>"
+        };
